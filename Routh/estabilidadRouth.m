0765e6044b03c2aa393e8733e432dfd32034c431
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2022-05-28 17:58:49 -0300

    Routh - casos especiales

--- Routh/estabilidadRouth.m
+++ Routh/estabilidadRouth.m
@@ -14,6 +14,43 @@ xlabel('Tiempo')
 legend('C(t)')
 
 figure(2)
-pzmap(FTLC)%%Devuelve los polos y ceros
+pzmap(FTLC)% Devuelve los polos y ceros
 
+%% Caso Especial N°1
+
+clc
+num=[1];
+den=[1 2 1 2];
+FTLC = tf(num,den)
+
+figure(1)
+impulse(FTLC)
+xlim([0 10])
+ylim([-2 2])
+title('Respuesta del sistema a una entrada Impulso - CE N°1')
+ylabel('Amplitud')
+xlabel('Tiempo')
+legend('C(t)')
+
+figure(2)
+pzmap(FTLC)% Devuelve los polos y ceros
+
+%% Caso Especial N°2
+
+clc
+num=[1];
+den=[1 2 24 48 -25 -50];
+FTLC = tf(num,den)
+
+figure(1)
+impulse(FTLC)
+xlim([0 10])
+ylim([-2 2])
+title('Respuesta del sistema a una entrada Impulso - CE N°2')
+ylabel('Amplitud')
+xlabel('Tiempo')
+legend('C(t)')
+
+figure(2)
+pzmap(FTLC)% Devuelve los polos y ceros
 
