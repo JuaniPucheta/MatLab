84e37b7910e7535481f60dc9f9217ed1543d5fb8
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-12-04 12:47:45 -0300

    add ej's FINALES

--- 2023/Ejercicios_Practica/Ej_21_Practica.m
+++ 2023/Ejercicios_Practica/Ej_21_Practica.m
@@ -4,3 +4,15 @@ den = [1 6 11 6];
 FTLA = tf(num, den);
 rlocus(FTLA);
     % si K>60, el sistema se vuelve inestable
+
+%% Otra forma con zp2tf
+k = 1;
+z = '';
+p = [-1 -2 -3];
+[num, den] = zp2tf(z,p,k);
+FTLA = tf(num, den);
+
+figure(1)
+    rlocus(FTLA)
+    title('Polos y Ceros de FTLA');
+    ylabel('Amplitud'); xlabel('Tiempo')
